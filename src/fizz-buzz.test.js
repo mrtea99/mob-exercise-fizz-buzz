ad009c9a4d69b6e12c3ac7d2c5cfe2f69972cae3
Stephen Train <stephen.train@sky.uk>
2024-03-19 15:35:23 +0000

    stephen driving

--- src/fizz-buzz.test.js
+++ src/fizz-buzz.test.js
@@ -2,20 +2,39 @@ const fizzBuzz = (number) => {
   if (isNaN(number)) {
     return false;
   }
+
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'FizzBuzz';
+  }
+
+  if (number % 3 === 0) {
+    return 'Fizz';
+  }
+
+  if (number % 5 === 0) {
+    return 'Buzz';
+  }
 };
 
 describe('Fizz Buzz', () => {
   it('return false when the given argument is not a number', () => {
     const result = fizzBuzz('gobbledygook');
     expect(result).toBe(false);
-    //call the fuction withe number
-    // call function as string
-    // Arrange
-    // Act
-    // Assert
   });
   it('Returns "Fizz" if the number is divisible by 3', () => {
     const result = fizzBuzz(9);
     expect(result).toBe('Fizz');
+  });
+  it('Returns "Buzz" if the number is divisible by 5', () => {
+    const result = fizzBuzz(10);
+    expect(result).toBe('Buzz');
+  });
+  it('Returns "FizzBuzz" if the number is divisible by 3 AND 5', () => {
+    const result = fizzBuzz(15);
+    expect(result).toBe('FizzBuzz');
+  });
+  it('Returns the given number if it is not divisible by 3 OR 5', () => {
+    const result = fizzBuzz(7);
+    expect(result).toBe(7);
   });
 });
