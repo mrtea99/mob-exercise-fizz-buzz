9b5ed984e8c9d6c5f6dc42623091370ffe33a45e
Stephen Train <stephen.train@sky.uk>
2024-03-19 11:50:42 +0000

    mark's stuff

--- src/fizz-buzz.test.js
+++ src/fizz-buzz.test.js
@@ -1,8 +1,13 @@
-const prod_code = (number) => {};
+const fizzBuzz = (number) => {
+  if (isNaN(number)) {
+    return false;
+  }
+};
 
 describe('Fizz Buzz', () => {
   it('return false when the given argument is not a number', () => {
-    // const return = ;
+    const result = fizzBuzz('gobbledygook');
+    expect(result).toBe(false);
     //call the fuction withe number
     // call function as string
     // Arrange
